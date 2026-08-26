34d9185f7a0d8bd3f33108f18566a169b4dbaacb
Moth-Logic <juliansolorzano.hernandez@gmail.com>
2026-08-26 09:49:06 -0600

    Deleted Stuff and Updated Pilas Colas

---
+++ Pilas y Colas_Juli/LinkedStack.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include "Stack.h"
+#include "Node.h"
+
+using std::runtime_error;
+using std::cout;
+using std::endl;
+
+template<typename E>
+class LinkedStack : public Stack<E> {
+private:
+	Node<E>* top;
+	int size;
+
+public:
+	LinkedStack() {
+		top = nullptr;
+		size = 0;
+	}
+	~LinkedStack() {
+		clear();
+	}
+	void push(E element) {
+		top = new Node<E>(element, top);
+		size++;
+	}
+	E pop() {
+		if (size == 0)
+			throw runtime_error("Stack is empty");
+		E result = top->element;
+		Node<E>* temp = top->next;
+		delete top;
+		top = temp;
+		size--;
+		return result;
+	}
+	E topValue() {
+		if (size == 0)
+			throw runtime_error("Stack is empty");
+		return top->element;
+	}
+	void clear() {
+		Node<E>* temp;
+		while (top != nullptr) {
+			temp = top->next;
+			delete top;
+			top = temp;
+		}
+		size = 0;
+	}
+	bool isEmpty() {
+		return size == 0;
+	}
+	int getSize() {
+		return size;
+	}
+	void print() {
+		cout << "[";
+		Node<E>* temp = top;
+		while (temp != nullptr) {
+			cout << temp->element;
+			if (temp->next!=nullptr)
+				cout << ", ";
+			temp = temp->next;
+		}
+		cout << "]" << endl;
+	}
+};
--- Pilas y Colas_Juli/Stack.h
+++ Pilas y Colas_Juli/Stack.h
@@ -2,19 +2,18 @@
 
 template <typename E>
 class Stack {
-private:
-	Stack(const Stack<E> &other) {}
-	void operator =(const Stack<E>& other) {}
-
 public:
-	Stack() {}
+	Stack(){}
+	Stack(const Stack<E>&) = delete;
+	void operator = (const Stack<E>&) = delete;
 	virtual ~Stack() {}
 	virtual void push(E element) = 0;
 	virtual E pop() = 0;
-	virtual topValue() = 0;
+	virtual E topValue() = 0;
 	virtual void clear() = 0;
 	virtual bool isEmpty() = 0;
 	virtual int getSize() = 0;
 	virtual void print() = 0;
 };
+
 
